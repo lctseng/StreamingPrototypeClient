d6806882674398acf273914021d535687df0cb3b
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2017-01-22 17:20:30 +0800

    init commit

---
+++ android/src/com/covart/streaming_prototype/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.covart.streaming_prototype;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.covart.streaming_prototype.StreamingPrototype;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StreamingPrototype(), config);
+	}
+}
