a71297df89e907811d4a849237fc965e4d96564a
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2017-02-23 11:27:44 +0800

    add some stub for JNI H264

--- android/src/com/covart/streaming_prototype/ImageDecoderH264.java
+++ android/src/com/covart/streaming_prototype/ImageDecoderH264.java
@@ -31,15 +31,12 @@ public class ImageDecoderH264 extends ImageDecoderBase {
 
     @Override
     public void run() {
-        MediaFormat format = new MediaFormat();
-        format.setString(MediaFormat.KEY_MIME, "video/avc");
-        format.setInteger(MediaFormat.KEY_MAX_INPUT_SIZE, 100000);
-        format.setInteger(MediaFormat.KEY_WIDTH, 399);
-        format.setInteger(MediaFormat.KEY_HEIGHT, 600);
-        format.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_Format24bitRGB888);
+        MediaFormat mediaFormat = MediaFormat.createVideoFormat("video/avc", 399, 600);
+        mediaFormat.setString(MediaFormat.KEY_MIME, "video/avc");
+        mediaFormat.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_Format24bitRGB888);
         try {
             codec = createDecoderByType("video/avc");
-            codec.configure(format, null, null, 0);
+            codec.configure(mediaFormat, null, null, 0);
             codec.start();
         } catch (IOException e) {
             e.printStackTrace();
@@ -53,13 +50,14 @@ public class ImageDecoderH264 extends ImageDecoderBase {
                 try {
                     // read from network
                     Profiler.reportOnProcStart();
+                    Gdx.app.log("H264","Getting input from network");
                     int inputBufferId = codec.dequeueInputBuffer(-1);
                     if (inputBufferId >= 0) {
                         // retrieve data from netowrk
                         Buffer encodedBuf = acquireEncodedResult();
                         Gdx.app.log("H264", "Raw H264 stream, size: " + encodedBuf.size);
                         inputBuffers[inputBufferId].rewind();
-                        inputBuffers[inputBufferId].put(encodedBuf.data);
+                        inputBuffers[inputBufferId].put(encodedBuf.data,0, encodedBuf.size);
                         // fill inputBuffers[inputBufferId] with valid data
                         codec.queueInputBuffer(inputBufferId, 0 , encodedBuf.size, 40000 , 0);
                         // release buffer to network
@@ -67,14 +65,18 @@ public class ImageDecoderH264 extends ImageDecoderBase {
                     }
                     // store to display
                     // get image buffer if can
-
-                    int outputBufferId = codec.dequeueOutputBuffer(info, -1);
+                    Gdx.app.log("H264","Getting output from H264");
+                    int outputBufferId = codec.dequeueOutputBuffer(info, 0);
                     if (outputBufferId >= 0) {
                         // outputBuffers[outputBufferId] is ready to be processed or rendered.
                         // get display buffer
                         Buffer decodeBuf = acquireImageBuffer();
                         // copy data
-                        outputBuffers[outputBufferId].get(decodeBuf.data);
+                        int offset = 0 ;
+                        while(outputBuffers[outputBufferId].hasRemaining()){
+                            outputBuffers[outputBufferId].get(decodeBuf.data, offset, 1);
+                            offset += 1;
+                        }
                         decodeBuf.size = outputBuffers[outputBufferId].position();
                         Gdx.app.log("H264", "Decoded image data, size: " + decodeBuf.size);
                         // send to display
