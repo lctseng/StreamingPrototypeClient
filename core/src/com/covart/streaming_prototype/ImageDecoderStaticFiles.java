85cfa1b1fab6633cb9320aadb75e262117365948
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2017-03-11 13:06:40 +0800

    add static file decoder for test purpose

---
+++ core/src/com/covart/streaming_prototype/ImageDecoderStaticFiles.java
@@ -0,0 +1,59 @@
+package com.covart.streaming_prototype;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Pixmap;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * Created by lctseng on 2017/2/11.
+ * NTU COV-ART Lab, for NCP project
+ */
+
+public class ImageDecoderStaticFiles extends ImageDecoderBase {
+
+    @Override
+    public void run() {
+        // open all files
+        FileHandle dirHandle = Gdx.files.internal("lightfield");;
+        ArrayList<FileHandle> files = new ArrayList<FileHandle>();
+        Collections.addAll(files, dirHandle.list());
+        int max_size = files.size();
+        int count = 0;
+
+        while(true){
+            try {
+                // does not care about data from network
+                // non-blocking read from network
+                Buffer encodedBuf = BufferPool.getInstance().queueNetworkToDecoder.poll();
+                if(encodedBuf != null){
+                    // blocking return to network
+                    BufferPool.getInstance().queueDecoderToNetwork.put(encodedBuf);
+                }
+                Thread.sleep(10);
+                if(count >= max_size){
+                    count = 0;
+                }
+                FileHandle file = files.get(count);
+                count += 1;
+                if(file != null){
+                    Pixmap img = new Pixmap(file);
+                    ByteBuffer pixels = img.getPixels();
+                    Buffer decodeBuf = acquireImageBuffer();
+                    // copy to buffer
+                    decodeBuf.size = pixels.remaining();
+                    pixels.get(decodeBuf.data, 0, decodeBuf.size);
+                    // send back
+                    sendImageResult(decodeBuf);
+                }
+
+            } catch (InterruptedException e) {
+                Gdx.app.error("Decoder", "Worker interrupted");
+                break;
+            }
+        }
+    }
+}
