906987779242438564b5e490cca68cd3268c0578
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2017-04-28 20:45:33 +0800

    change to 256x256

--- core/src/com/covart/streaming_prototype/DisplayLightField.java
+++ core/src/com/covart/streaming_prototype/DisplayLightField.java
@@ -24,7 +24,7 @@ public class DisplayLightField extends DisplayBase{
     final static int COL_WIDTH = 16;
     final static int ROW_WIDTH = 4;
     final static int TOTAL_IMAGES = COL_WIDTH * ROW_WIDTH;
-    final static int DIMENSION = 512;
+    final static int DIMENSION = 256;
 
     final static int HALF_COL_SPAN = 1;
 
