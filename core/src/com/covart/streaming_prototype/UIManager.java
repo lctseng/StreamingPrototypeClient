a3bf4ecbde2e046ea4e94abb082e2083ae9b9de4
Liang-Chi Tseng <lctseng.github@cs.nctu.edu.tw>
2017-04-29 13:39:14 +0800

    add UI manager skeleton

---
+++ core/src/com/covart/streaming_prototype/UIManager.java
@@ -0,0 +1,33 @@
+package com.covart.streaming_prototype;
+
+import com.badlogic.gdx.utils.Disposable;
+
+/**
+ * Created by lctseng on 2017/4/29.
+ * NTU COV-ART Lab, for NCP project
+ */
+
+class UIManager implements Disposable{
+
+    private static UIManager ourInstance;
+
+    public static UIManager getInstance() {
+        return ourInstance;
+    }
+
+    public static void initialize(){
+        ourInstance = new UIManager();
+    }
+
+    public static void cleanup(){
+        ourInstance.dispose();
+    }
+
+    private UIManager() {
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
